2b3bed6d9c70bf4921df48ddb1b7f3374d00d533
Lucas Losekann <lucas1losekann@gmail.com>
2024-11-05 00:00:33 -0300

    Remove Footer component from Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,9 @@
-import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function Index() {
     return (
         <div className="w-full">
             <Header />
-            <Footer />
         </div>
     )
 }
